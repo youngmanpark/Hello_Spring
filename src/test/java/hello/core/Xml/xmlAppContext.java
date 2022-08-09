39a0a92f6f95d2d5ff0e5cc610da724e31b135d9
YeongMin Park <py980627@skuniv.ac.kr>
2022-08-10 00:14:23 +0900

    update beandefinition

---
+++ src/test/java/hello/core/Xml/xmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.Xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class xmlAppContext  {
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac= new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
