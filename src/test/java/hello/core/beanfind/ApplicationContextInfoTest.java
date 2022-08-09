92efaabff2730281c8c92b3d3deceb630372c75a
YeongMin Park <py980627@skuniv.ac.kr>
2022-08-09 12:50:39 +0900

    update beantest

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -3,9 +3,10 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContextinfoTest {
+public class ApplicationContextInfoTest {
 
     AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
 
@@ -14,7 +15,14 @@ public class ApplicationContextinfoTest {
     void findAllBean(){
         String[]beanDefinitionNames=ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
+            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            if(beanDefinition.getRole()==BeanDefinition.ROLE_APPLICATION){
+                Object bean=ac.getBean(beanDefinitionName);
+                System.out.println("name = "+beanDefinitionName+" object = "+bean);
+            }
         }
     }
 }
